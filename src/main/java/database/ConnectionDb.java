013e5afd5e584a25e9051a5ebbe6bd6c31ca59e3
alejandro <dsepulvedaa@unal.edu.co>
2022-06-12 18:58:19 -0500

    0.0.4
    montado el crud de insert para productos y usuarios.
    
    Una sola conexion para todo.

--- src/main/java/database/ConnectionDb.java
+++ src/main/java/database/ConnectionDb.java
@@ -3,7 +3,7 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConexionDb {
+public class ConnectionDb {
    public Connection conectar(){
        String host="database-1.cwvm1nkqoc9g.us-east-1.rds.amazonaws.com";
        String user="program";
@@ -21,8 +21,8 @@ public class ConexionDb {
             conec = DriverManager.getConnection("jdbc:postgresql://"+host+":5432/"+db,user,pass);
             boolean valido = conec.isValid(50000);
            System.out.println(valido?"test ok":"test fail");
-       }catch (java.sql.SQLException sql){
-           System.out.println(sql);
+       }catch (java.sql.SQLException e){
+           System.out.println(e);
        }
        return conec;
    }
