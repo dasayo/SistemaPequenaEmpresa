447b15ed51b5a4100f9b51632d138c09eec7988d
alejandro <dsepulvedaa@unal.edu.co>
2022-06-12 18:59:27 -0500

    0.0.4
    Montado de crudSelectProducts

---
+++ src/main/java/database/CrudSelecteProductos.java
@@ -0,0 +1,15 @@
+package database;
+
+import java.sql.Connection;
+
+public class CrudSelecteProductos {
+    private Connection connection;
+
+    public CrudSelecteProductos(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void allProducts(){
+
+    }
+}
