d62367939e12bde6d149e19757fb81fc5131e435
alejandro <dsepulvedaa@unal.edu.co>
2022-06-12 18:40:26 -0500

    0.0.3
    montado el crud de insert para productos y usuarios.
    
    Una sola conexion para todo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
-import database.ConexionDb;
+import database.ConnectionDb;
 import database.CrudInsert;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        CrudInsert insert = new CrudInsert();
-        insert.insertarPr("lentejas",5,"02/15/2024",2450.99f);
-        insert.insertarUs(1037660985,"dasayo","1235","David");
+        ConnectionDb connectionDb = new ConnectionDb();
+        Connection con= connectionDb.conectar();
+        CrudInsert insert = new CrudInsert(con);
+        insert.insertarPr("garbanzo",10,"2023-3-22",3500.22f);
+        insert.insertarUs(15457413,"Lsepulv","sdea","Luis");
     }
 }
