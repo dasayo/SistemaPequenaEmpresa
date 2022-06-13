5e5e4cfae334fc9345a8576a0d1aea874fde5253
alejandro <dsepulvedaa@unal.edu.co>
2022-06-12 23:00:33 -0500

    0.0.5
    Montado de crudSelectProducts
    Se puede filtrar por cada columna.
    Se puede hacer busquedas por columna y parametro(el valor que se quiera buscar segun la columna)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import database.ConnectionDb;
 import database.CrudInsert;
+import database.CrudSelecteProductos;
 
 import java.sql.Connection;
 
@@ -8,7 +9,11 @@ public class Main {
         ConnectionDb connectionDb = new ConnectionDb();
         Connection con= connectionDb.conectar();
         CrudInsert insert = new CrudInsert(con);
-        insert.insertarPr("garbanzo",10,"2023-3-22",3500.22f);
-        insert.insertarUs(15457413,"Lsepulv","sdea","Luis");
+        //insert.insertarPr("garbanzo",10,"2023-3-22",3500.22f);
+        //insert.insertarUs(15457413,"Lsepulv","sdea","Luis");
+        CrudSelecteProductos productos =new CrudSelecteProductos(con);
+        //productos.filtro(true,"vencimiento");
+        productos.search("5","cantidad");
+
     }
 }
--- src/main/java/database/CrudSelecteProductos.java
+++ src/main/java/database/CrudSelecteProductos.java
@@ -1,6 +1,10 @@
 package database;
 
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CrudSelecteProductos {
     private Connection connection;
@@ -10,6 +14,80 @@ public class CrudSelecteProductos {
     }
 
     public void allProducts(){
+        String sql = "select * from productos;";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                int cantidad = resultSet.getInt(3);
+                float precio = resultSet.getFloat(4);
+                String vencimiento = String.valueOf(resultSet.getDate(5));
+                System.out.println(""+id+"---"+name+"---"+cantidad+"---"+precio+"---"+vencimiento);
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
 
     }
+
+    public void filtro(boolean check, String column){
+
+        switch (column){
+            case "id":
+            case "name":
+            case "precio":
+            case "cantidad":
+            case "vencimiento":
+                String sql = check?"SELECT * FROM productos ORDER BY "+column+" ASC;":"SELECT * FROM productos ORDER BY "+
+                        column+" DESC;";
+                try {
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(sql);
+                    while (resultSet.next()){
+                        int id = resultSet.getInt(1);
+                        String name = resultSet.getString(2);
+                        int cantidad = resultSet.getInt(3);
+                        float precio = resultSet.getFloat(4);
+                        String vencimiento = String.valueOf(resultSet.getDate(5));
+                        System.out.println(""+id+"---"+name+"---"+cantidad+"---"+precio+"---"+vencimiento);
+                    }
+                }catch (SQLException e){
+                    System.out.println(e);
+                }
+                break;
+            default:
+                System.out.println("no se ingreso una columna valida");
+        }
+    }
+
+    public void search(String busqueda, String column){
+        switch (column) {
+            case "id":
+            case "name":
+            case "precio":
+            case "cantidad":
+            case "vencimiento":
+                String sqlStatement = "SELECT * FROM productos WHERE " + column + "=" + busqueda + ";";
+                try {
+                    Statement statement = connection.createStatement();
+                    ResultSet resultado = statement.executeQuery(sqlStatement);
+                    while (resultado.next()){
+                        int id = resultado.getInt(1);
+                        String name = resultado.getString(2);
+                        int cantidad = resultado.getInt(3);
+                        float precio = resultado.getFloat(4);
+                        String vencimiento = String.valueOf(resultado.getDate(5));
+                        System.out.println(""+id+"---"+name+"---"+cantidad+"---"+precio+"---"+vencimiento);
+                    }
+                }catch (SQLException e){
+                    System.out.println(e);
+                }
+                break;
+            default:
+                System.out.println("columna no valida");
+        }
+    }
+
 }
